4db1320bdcfe56af6c053ad4dd145359c46c00c9
ommanirai <ommanirai27@gmail.com>
2024-09-15 08:09:42 +0545

    js project

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+/*
+1995
+ryan dahl => 2009
+
+node js
+framework
+library => 
+run time environment
+
+npm => node package manager
+
+file-file communication
+=> one file must export
+=> another file must import
+
+
+
+*/
