2172421448fab296725728e14ff5cc03d4c28129
ommanirai <ommanirai27@gmail.com>
2024-09-23 07:26:09 +0545

    middleware

--- backend/app.js
+++ backend/app.js
@@ -34,18 +34,55 @@ app.get("/login", function (request, response) {
     download()
     render()
     sendFile()
-    
-    
     */
 })
 
-app.get("/write/:filename/:content",function(request,response){
+app.get('/user/update/:id', function (req, res) {
+    // res.json({
+    //     msg: "from user update",
+    //     user_id: req.params.id,
+    //     username: req.query.username,
+    //     password: req.query.password
+    // })
+    var reg_username = 'user'
+    var reg_password = 'user123'
+    if (req.query.username == reg_username && req.query.password == reg_password) {
+        res.json({
+            msg: "logged in successfully",
+            status: 200
+        })
+    }
+    else {
+
+    }
+})
+
+app.get("/write/:filename/:content", function (request, response) {
     // response.json({
     //     filename: request.params
     // })
     response.json(request.params)
 })
 
+// app.get("*", function (req, res) {
+//     res.json({
+//         msg: "Page Not Found",
+//         status: 404
+//     })
+// })
+
+// ragardless of any method and any url
+var mid = function (req, res) {
+    res.json({
+        msg: "Page Not Found",
+        status: 404
+    })
+}
+app.use(mid, function (req, res) {
+
+}, function (req, res) {
+
+})
 
 app.listen(port, function (err, done) {
     if (err) {
@@ -55,3 +92,23 @@ app.listen(port, function (err, done) {
         console.log("server listening at port: ", port);
     }
 })
+
+// middleware
+/*
+function(req, res, next){
+
+}
+
+
+
+app.use(middleware)
+
+
+app.use(function(req,res, next){
+})
+
+=> middleware is a function which has access to request object, response object and next middleware function reference
+=> middleware always came into action in between req-res cycle
+=> middleware can modify request object 
+
+*/
